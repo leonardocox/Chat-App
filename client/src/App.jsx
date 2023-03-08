07f20a75dbe17e467face79fb9ee58af870b3142
leonardocox <leonardocx0@gmail.com>
2023-03-08 18:25:15 -0500

    setup .env.local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-
-  return (
+  return ( 
     <div className="app">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
