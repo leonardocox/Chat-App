f6257e47d7d4503824e7b8ad0d8df87f0a3f6a1a
leonardocox <leonardocx0@gmail.com>
2023-03-09 11:21:37 -0500

    standard message form for chat custom message form being fleshed out

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const StandardMessageForm = () => {
-  return <div>StandardMessageForm</div>;
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            alt="message-form-preview"
+            className="message-form-preview-image"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StandardMessageForm;
