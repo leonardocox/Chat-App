012bffaeaf1e2e9a27dca6f650c44eb8580f1b83
leonardocox <leonardocx0@gmail.com>
2023-03-10 10:45:33 -0500

    fixed quick variable issue

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 /* OPEN AI CONFIGURATION */
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPEN_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
