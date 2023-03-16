99192b2f57223114ecb14bfc13df1c6295d34b4d
leonardocox <leonardocx0@gmail.com>
2023-03-15 20:22:38 -0400

    very sick last few days, so only added a bit. Adding POST with redux query

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -4,5 +4,13 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    postAiText: build.mutation({
+      query: (payload) => ({
+        url: "openai/text",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+  }),
 });
