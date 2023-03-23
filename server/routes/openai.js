e1e6cad602d9091216d45d8ecae294f5ea47fea4
leonardocox <leonardocx0@gmail.com>
2023-03-23 00:31:30 -0400

    finished AI code and ai can now answer questions or respond to you however you like

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -9,7 +9,6 @@ const router = express.Router();
 router.post("/text", async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
-    console.log("req.body:", req.body);
 
     // Open AI and how we make an API call
     const response = await openai.createCompletion({
@@ -21,7 +20,40 @@ router.post("/text", async (req, res) => {
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
-    console.log("response data", response.data);
+
+    await axios.post(
+      `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+      { text: response.data.choices[0].text },
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": process.env.BOT_USER_NAME,
+          "User-Secret": process.env.BOT_USER_SECRET,
+        },
+      }
+    );
+
+    res.status(200).json({ text: response.data.choices[0].text });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/code", async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    // Open AI and how we make an API call
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: text,
+      temperature: 0.5,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
 
     await axios.post(
       `https://api.chatengine.io/chats/${activeChatId}/messages/`,
